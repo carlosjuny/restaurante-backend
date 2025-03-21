0a0acffd291b7b4b683e032fecc2128d702b36e9
carlosjuny <carlosjuni.medina@gmail.com>
2025-03-21 01:01:11 -0500

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,5 +15,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-
+        //
     })->create();
